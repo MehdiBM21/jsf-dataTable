41b35b425a5006a309c7473383c8a698af850bd0
Mehdi <mehdibaallal@gmail.com>
2024-03-09 15:07:49 +0100

    final version

--- src/main/java/com/example/demo14/dao/ConnectionDb.java
+++ src/main/java/com/example/demo14/dao/ConnectionDb.java
@@ -13,8 +13,7 @@ public class ConnectionDb {
             // Load the JDBC driver
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            connection = DriverManager.getConnection("jdbc:mysql://mysql-37ee7df0-mehdibm-b7be.a.aivencloud.com:22846/defaultdb", "avnadmin", "AVNS_3cj9R0sBwi6h5V0Ancs");
-//                        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/servlet", "root", "");
+                        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/servlet", "root", "");
             System.out.println("Connexion établie");
         }catch(SQLException | ClassNotFoundException e){
             e.printStackTrace();
